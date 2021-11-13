f45e7376140ec9a0c4e64f1109cca0e42e902bc6
Col0ring <1561999073@qq.com>
2021-11-13 19:23:45 +0800

    feat: add node template

--- src/repositories.js
+++ src/repositories.js
@@ -2,9 +2,15 @@ module.exports = {
   Vue: {},
   React: {
     'umi-admin-template':
-      'https://github.com/Col0ring/umi-admin-template.git#master'
+      'https://github.com/Col0ring/umi-admin-template.git#master',
+    'react-ts-quick-starter':
+      'https://github.com/Col0ring/react-ts-quick-starte.git#main',
+    'react-ts-quick-starter':
+      'https://github.com/Col0ring/react-ts-quick-starte.git#antd'
   },
-  'Node.js': {},
+  'Node.js': {
+    'express-ioc': 'https://github.com/Col0ring/express-ioc.git#main'
+  },
   Electron: {
     'create-react-app + typescript + electron':
       'https://github.com/Col0ring/ts-react-electron-template.git#master',
